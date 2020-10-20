ba6eb7de32ff561eff674563524078efc74baecf
jinzi.yuan <jinzi.yuan.com>
2020-10-20 15:27:22 +0800

    update

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import Test from '../test'
+import Test from "../test";
 
 const Home = () => {
-return <div>这是首页组件<Test></Test>
-</div>;
+  return (
+    <div>
+      这是首页组件<Test></Test>
+    </div>
+  );
 };
 
 export default Home;
